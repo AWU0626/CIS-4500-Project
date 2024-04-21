83c54ddc8ccc44867b2ca16428b8a0ade09db62e
Jesse Zong <jessej.zong@gmail.com>
2024-04-21 15:39:28 -0400

    More boilerplate for routes

--- server/server.js
+++ server/server.js
@@ -10,7 +10,16 @@ app.use(cors({
 
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
-app.get('/', routes.mainpage)
+app.get('/api/', routes.mainpage)
+app.get('/api/areas/cities/education/', routes.query1And2)
+app.get('/api/houses/growing/', routes.query3)
+app.get('/api/schools/ratio/', routes.query4)
+app.get('/api/areas/cities/recommended/', routes.query5)
+app.get('/api/schools/recommended/', routes.query6)
+app.get('/api/areas/zips/recommended/', routes.query7)
+app.get('/api/areas/zips/occupancy/', routes.query8)
+app.get('/api/houses/', routes.query9)
+app.get('/api/areas/zips/prices/', routes.query10)
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
