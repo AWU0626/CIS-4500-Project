0ac5f365254d0e353f42983659823eaa540f478c
Jesse Zong <jessej.zong@gmail.com>
2024-04-30 12:34:56 -0400

    Fix query 2 and implement query 9

--- server/routes.js
+++ server/routes.js
@@ -41,7 +41,7 @@ const query2 = async function (req, res) {
   with specific_area as
   (select *
    from EDUCATION
-   where EDUCATION.STATE = '%${state}%' and EDUCATION.AREA_NAME = '%${area}%')
+   where EDUCATION.STATE = '${state}' and EDUCATION.AREA_NAME = '${area}')
 select e1.ID as ID, e1.STATE as STATE, e1.AREA_NAME as AREA_NAME, e1.TIME as TIME, e1.COUNT as hs_below, e2.COUNT as hs, e3.COUNT as 4_below, e4.COUNT as 4_above
 from specific_area e1 join specific_area e2 join specific_area e3 join specific_area e4 on e1.STATE = e2.STATE and e2.STATE = e3.STATE and e3.STATE = e4.STATE and e1.AREA_NAME = e2.AREA_NAME and e2.AREA_NAME = e3.AREA_NAME and e3.AREA_NAME = e4.AREA_NAME and e1.TIME = e2.TIME and e2.TIME = e3.TIME and e3.TIME = e4.TIME and e1.EDUCATION_LEVEL='hs_below' and e2.EDUCATION_LEVEL='hs' and e3.EDUCATION_LEVEL='4_below' and e4.EDUCATION_LEVEL='4_above'
 order by e1.TIME, e1.STATE, e1.AREA_NAME
@@ -109,7 +109,11 @@ const query3 = async function (req, res) {
 
 // Route 4: GET /query4
 const query4 = async function (req, res) {
-  res.status(200).json({ message: 'query 4!' })
+  const state = req.query.state
+  const city = req.query.city
+  const startGrade = req.query.start_grade ? req.query.start_grade : 1;
+  const endGrade = req.query.end_grade ? req.query.end_grade : 12;
+  const
 }
 
 // Route 5: GET /query5
@@ -307,7 +311,28 @@ const query8 = async function (req, res) {
 
 // Route 9: GET /query9
 const query9 = async function (req, res) {
-  res.status(200).json({ message: 'query 9!' })
+  const minPrice = req.query.min_price ? req.query.min_price : 0;
+  const maxPrice = req.query.max_price ? req.query.max_price : 1000000000;
+  const minBaths = req.query.min_baths ? req.query.min_baths : 0;
+  const minBeds = req.query.min_beds ? req.query.min_beds : 0;
+  const state = req.query.state
+  const page = req.query.page ?? 1;
+  const query = `select *
+from REAL_ESTATE
+where PRICE between ${minPrice} AND ${maxPrice}
+AND STATE = '${state}'
+AND BED >= ${minBeds}
+AND BATH >= ${minBaths}
+ORDER BY PRICE;
+LIMIT 10 OFFSET ${(page - 1) * 10}`
+    connection.query(query, (err, data) => {
+        if (err) {
+        console.log(err)
+        res.status(500).json({ error: 'Failed to query database' })
+        } else {
+        res.status(200).json(data)
+        }
+    })
 }
 
 // Route 10: GET /query10
